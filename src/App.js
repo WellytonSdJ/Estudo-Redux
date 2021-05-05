8bbf7c8fac43781dba1a9b440bf0e1f4a64952c3
Wellyton Sadlovski de Jesus <wellyton@dotse.com.br>
2021-05-05 18:58:56 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Contador from './Contador'
+import Counter from './features/Counter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <h1>APRENDENDO REDUX</h1>
+      {/* <Contador /> */}
+      <Counter />
+    </center>
   );
 }
 
---
+++ src/Contador/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Contador = () => {
+    return (
+        <div>
+            <h1>Counter App</h1>
+            <p>Contando: </p>
+            <button>Aumentar</button><br />
+            <button>Diminuir</button><br />
+            <button>
+                Change by value
+            </button><br />
+            <input />
+        </div>
+    )
+}
+
+export default Contador;
